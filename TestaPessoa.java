92eb7ac20461e0601962d65168c2cbeaff713865
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-17 18:52:10 -0300

    feat: unidade i

--- TestaPessoa.java
+++ TestaPessoa.java
@@ -1,13 +1,52 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class TestaPessoa {
+
+    public static Pessoa recebePessoa() {
+
+        Pessoa p = null;
+        String nome;
+        int telefone = 0;
+
+        nome = JOptionPane.showInputDialog("Qual e o nome da pessoa ?");
+
+        // laço para receber o telefone até que um valor correto tenha sido inserido;
+        boolean repete = true;
+
+        do {
+            try {
+                telefone = Integer.parseInt(JOptionPane.showInputDialog("Qual é o telefone de " + nome + "?"));
+                repete= false;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Digite apenas números. Tente outra vez");
+            }
+        } while (repete);
+        p = new Pessoa(nome, telefone);
+        return p;
+        
+
+    }
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, 0);
-        p1.setNome("Manoel");
-        p1.setTelefone(123456789);
-        p1.apresente();
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
 
-        Pessoa p2 = new Pessoa("Joaquim", 987654321);
-        p2.apresente();
-    }
+        // Recebimento dos dados das pessoas:
+
+        int continua;
+
+        do {
+            pessoas.add(recebePessoa());
+            continua = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Pergunta", JOptionPane.YES_NO_OPTION);
+        } while (continua == JOptionPane.YES_OPTION);
+
+        // exibição dos dados das pessoas:
+
+        for (Pessoa p : pessoas) {
+            p.apresente();
+        }
+    } 
+    
 }
